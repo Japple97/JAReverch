642dd00ffbc1e984910f70db021caada9b5413c7
Japple97 <applejake9@gmail.com>
2023-01-18 15:14:17 -0500

    publishedCommit

--- js/site.js
+++ js/site.js
@@ -1,12 +1,28 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
+//entry point AKA controller
+function getValues() {
+    let inputString = document.getElementById('userString').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-
-        }
-    );
+    let reversedString = reverseAString(inputString);
+    displayString(reversedString);
 }
+
+//logic function
+//reverse a string
+function reverseAString(userString) {
+    let revString = '';
+
+    for(let i = userString.length - 1; i >= 0; i = i - 1) {
+        revString += userString[i];
+    }
+
+
+
+    return revString; //some string;
+}
+
+//view function
+function displayString(revString) {
+    document.getElementById('results').textContent = revString;
+    document.getElementById('alert').classList.remove('invisible');
+}
+
